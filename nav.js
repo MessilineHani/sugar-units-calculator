f7a17ef3732d79058198c202e2503761550bcba8
MessilineHani <pleasefocus.exe@gmail.com>
2023-09-16 19:36:56 +0100

    menu

--- nav.js
+++ nav.js
@@ -11,17 +11,31 @@ menuBtn.style.margin = "auto 5%";
 menuBtn.style.display = "none";
 const navbar = document.getElementById("navbar");
 navbar.appendChild(menuBtn);
+const navlist = document.querySelectorAll(".navlist");
 function callMenu (){
-    const navlist = document.querySelectorAll(".navlist");
     if(window.innerWidth <= 767){
         menuBtn.style.display = "block";
         navlist.forEach(el =>{
             el.style.display = "none"
+            menuBtn.onclick = () => {
+                if(el.style.display == "none"){
+                    el.style.display = "block";
+                    menuBtn.src = "../footage/images/minus.png"
+                }
+                else{
+                    el.style.display = "none"
+                    menuBtn.src = "../footage/images/menu.png"
+                }
+            }
         })
     }else{
         menuBtn.style.display = "none"
         navlist.forEach(el =>{
             el.style.display = "block"
+            menuBtn.onclick = () =>{
+                el.style.display = "none"
+                menuBtn.scr = "../footage/images/menu.png"
+            }
         })
     }
 }
